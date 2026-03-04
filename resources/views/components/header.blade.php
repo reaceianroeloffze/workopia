a51b06c5dca1244b2fd34000716a1dc945e9b99f
Reace Ian Roeloffze <reaceianroeloffze@gmail.com>
2026-03-04 21:43:44 +0200

    Add nav-link and button-link components to mobile menu

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -59,32 +59,50 @@
     <nav id="mobile-menu"
          class="hidden md:hidden bg-blue-900 text-white mt-5 pb-4 space-y-2"
     >
-        <a href="{{url('/jobs')}}"
-           class="block px-4 py-2 hover:bg-blue-700"
-        >All Jobs</a
+        <x-nav-link url="/jobs"
+                    :active="request()->is('jobs')"
+                    :mobile="true"
+                    title="View all jobs currently available on Workopia"
         >
-        <a
-                href="{{url('/jobs/saved')}}"
-                class="block px-4 py-2 hover:bg-blue-700"
-        >Saved Jobs</a
+            All Jobs
+        </x-nav-link>
+        <x-nav-link url="/jobs/saved"
+                    :active="request()->is('jobs/saved')"
+                    :mobile="true"
+                    title="Your saved/bookmarked jobs"
         >
-        <a href="{{url('/login')}}" class="block px-4 py-2 hover:bg-blue-700"
-        >Login</a
+            Saved Jobs
+        </x-nav-link>
+        <x-nav-link url="/login"
+                    :active="request()->is('login')"
+                    :mobile="true"
+                    icon="user"
+                    title="Login to your Workopia account"
         >
-        <a href="{{url('/register')}}"
-           class="block px-4 py-2 hover:bg-blue-700"
+            Login
+        </x-nav-link>
+        <x-nav-link url="/register"
+                    :active="request()->is('register')"
+                    :mobile="true"
+                    title="Create an account on Workopia"
         >
             Register
-        </a>
-        <a href="{{url('/dashboard')}}"
-           class="block text-white hover:underline py-2"
+        </x-nav-link>
+        <x-nav-link url="/dashboard"
+                    :active="request()->is('dashboard')"
+                    :mobile="true"
+                    icon="gauge"
+                    title="Go to your Workopia dashboard"
         >
-            <i class="fa fa-gauge mr-1"></i> Dashboard
-        </a>
-        <a href="{{url('/jobs/create')}}"
-           class="block px-4 py-2 bg-yellow-500 hover:bg-yellow-600 text-black"
+            Dashboard
+        </x-nav-link>
+        <x-button-link url="/jobs/create"
+                       title="Create a new job listing"
+                       icon="edit"
+                       :mobile="true"
+                       :block="true"
         >
-            <i class="fa fa-edit"></i> Create Job
-        </a>
+            Create Job
+        </x-button-link>
     </nav>
 </header>
--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -3,16 +3,30 @@
         'url' => '/',
         'active' => false,
         'icon' => null,
+        'mobile' => null,
     ]
 )
 
-<a {{$attributes}}
-   href="{{$url}}"
-   class="text-white hover:underline py-2
+@if ($mobile)
+    <a {{$attributes}}
+       href="{{$url}}"
+       class="block px-4 py-2 hover:bg-blue-700
+       {{$active ? 'text-yellow-500 font-bold' : ''}}"
+    >
+        @if($icon)
+            <i class="fa fa-{{$icon}} mr-1"></i>
+        @endif
+        {{$slot}}
+    </a>
+@else
+    <a {{$attributes}}
+       href="{{$url}}"
+       class="text-white hover:underline py-2
         {{$active ? 'text-yellow-500 font-bold' : ''}}"
->
-    @if($icon)
-        <i class="fa fa-{{$icon}} mr-1"></i>
-    @endif
-    {{$slot}}
-</a>
+    >
+        @if($icon)
+            <i class="fa fa-{{$icon}} mr-1"></i>
+        @endif
+        {{$slot}}
+    </a>
+@endif
