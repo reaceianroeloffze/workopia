96a29731528728ba3ff60a39ded26f978788bff1
Reace Ian Roeloffze <reaceianroeloffze@gmail.com>
2026-03-04 12:05:09 +0200

    Add font-awesome and meta tags for author and description

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,13 +1,23 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
-          content="width=device-width, initial-scale=1.0">
+          content="width=device-width,
+                   initial-scale=1.0"
+    >
+    <meta name="author" content="Reace Ian Roeloffze">
+    <meta name="description" content="Workopia is a Laravel-built job-listing and management web application    ">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>
-        {{"$title | Workopia" ?? 'Workopia | Find and List Jobs'}}
+        {{$title ?? 'Workopia | Find and List Jobs'}}
     </title>
+    {{-- Enable font-awesome --}}
+    <link rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css"
+          integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw=="
+          crossorigin="anonymous"
+          referrerpolicy="no-referrer"/>
     {{-- Enable Tailwind CSS --}}
     @vite(
         [
