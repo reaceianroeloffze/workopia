216f0d703feb881dca345b5a4ec2f5a81cf0eeef
Reace Ian Roeloffze <reaceianroeloffze@gmail.com>
2026-03-16 13:59:55 +0200

    Display error messages and update input field styles

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -1,10 +1,36 @@
 <x-layout>
     <x-slot name="title">Create New Job</x-slot>
-    <h1>Create New Job1</h1>
+    <h1>Create New Job</h1>
     <form action="/jobs" method="POST">
         @csrf
-        <input type="text" name="job_title" placeholder="Job Name">
-        <input type="text" name="job_description" placeholder="Job Description">
-        <button type="submit">Create</button>
+        <div class="my-5">
+            <input class="bg-white p-2"
+                   type="text"
+                   name="title"
+                   value="{{old('title')}}"
+                   placeholder="Job Name"
+            >
+            @error('title')
+            <div class="text-red-500 mt-2 text-small">
+                {{$message}}
+            </div>
+            @enderror
+        </div>
+        <div class="mb-5">
+            <input class="bg-white p-2"
+                   type="text"
+                   name="description"
+                   value="{{old('description')}}"
+                   placeholder="Job Description"
+            >
+            @error('description')
+            <div class="text-red-500 mt-2 text-small">
+                {{$message}}
+            </div>
+            @enderror
+        </div>
+        <button type="submit">
+            Create
+        </button>
     </form>
 </x-layout>
