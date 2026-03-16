84790f7a55b8c2ed5adb3fa8696c9a027306ffdd
Reace Ian Roeloffze <reaceianroeloffze@gmail.com>
2026-03-16 13:06:10 +0200

    Add additional fields to job_listings table migration

---
+++ database/migrations/2026_03_09_140500_add_fields_to_job_listings_table.php
@@ -0,0 +1,76 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // Truncate job_listings table
+        DB::table('job_listings')->truncate();
+
+        Schema::table('job_listings', function (Blueprint $table) {
+            $table->integer('salary')->nullable();
+            $table->string('tags')->nullable();
+            $table->enum(
+                'job_type',
+                [
+                    'Full-Time',
+                    'Part-Time',
+                    'Contract',
+                    'Temporary',
+                    'Internship',
+                    'Volunteer',
+                    'Permanent',
+                    'Freelance',
+                    'On-Call',
+                ]
+            )->default('Full-Time');
+            $table->boolean('is_remote')->default(false);
+            $table->string('requirements')->nullable();
+            $table->string('benefits')->nullable();
+            $table->string('address')->nullable();
+            $table->string('city');
+            $table->string('state');
+            $table->string('zip_code')->nullable();
+            $table->string('contact_email');
+            $table->string('contact_phone')->nullable();
+            $table->string('company_name');
+            $table->string('company_description')->nullable();
+            $table->string('company_logo')->nullable();
+            $table->string('company_website')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('job_listings', function (Blueprint $table) {
+            $table->dropColumn([
+                'salary',
+                'tags',
+                'job_type',
+                'is_remote',
+                'requirements',
+                'benefits',
+                'address',
+                'city',
+                'state',
+                'zip_code',
+                'contact_email',
+                'contact_phone',
+                'company_name',
+                'company_description',
+                'company_logo',
+                'company_website',
+            ]);
+        });
+    }
+};
