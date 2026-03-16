f0ea9c14b575bae4be5d61b4bc5ab584830d38b8
Reace Ian Roeloffze <reaceianroeloffze@gmail.com>
2026-03-16 19:10:09 +0200

    Revert JobListing to Job and include factory trait

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -4,9 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class JobListing extends Model
+class Job extends Model
 {
+    use HasFactory;
     protected $table = 'job_listings';
     protected $fillable = [
         'title',
