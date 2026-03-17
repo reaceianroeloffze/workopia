e89b1af4790b99d8870276c212da810825233e3a
Reace Ian Roeloffze <reaceianroeloffze@gmail.com>
2026-03-17 15:04:16 +0200

    Fix formatting

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Job extends Model
 {
     use HasFactory;
+
     protected $table = 'job_listings';
     protected $fillable = [
         'title',
