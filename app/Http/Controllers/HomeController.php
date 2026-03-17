e36d2f26526044668c8c859fabdffe0d3d9f1937
Reace Ian Roeloffze <reaceianroeloffze@gmail.com>
2026-03-17 15:02:48 +0200

    Display 6 latest jobs and update layout

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\View\View;
+use App\Models\Job;
 
 class HomeController extends Controller
 {
-    public function index()
+    public function index(): View
     {
-        return view('pages.index');
+        $jobs = Job::latest()->limit(6)->get();
+        return view('pages.index')->with('jobs', $jobs);
     }
 }
--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -1,13 +1,10 @@
 <x-layout>
     <x-slot name="title">Jobs</x-slot>
-    <h1>Available Jobs</h1>
-    <ul>
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
         @forelse ($jobs as $job)
-            <li><a href="{{route('jobs.show', $job->id)}}">
-                    {{$job->title}}
-                </a> - {{$job->description}}</li>
+            <x-job-card :job="$job"/>
         @empty
-            <li>No Available Jobs</li>
+            <p>No Available Jobs</p>
         @endforelse
-    </ul>
+    </div>
 </x-layout>
