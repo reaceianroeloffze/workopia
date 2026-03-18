298c5ba30db6c4f5bf076c508f77ff17c79a112c
Reace Ian Roeloffze <reaceianroeloffze@gmail.com>
2026-03-18 08:47:22 +0200

    Create input type file component

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -1,36 +1,162 @@
 <x-layout>
     <x-slot name="title">Create New Job</x-slot>
-    <h1>Create New Job</h1>
-    <form action="/jobs" method="POST">
-        @csrf
-        <div class="my-5">
-            <input class="bg-white p-2"
-                   type="text"
-                   name="title"
-                   value="{{old('title')}}"
-                   placeholder="Job Name"
+    <div class="bg-white
+                mx-auto
+                p-8
+                rounded-lg
+                shadow-md
+                w-full
+                md:max-w-3xl"
+    >
+        <h2 class="text-4xl text-center font-bold mb-4">
+            Create Job Listing
+        </h2>
+        <form method="POST"
+              action="/jobs"
+              enctype="multipart/form-data"
+        >
+            @csrf
+            <h2 class="text-2xl font-bold mb-6 text-center text-gray-500"
             >
-            @error('title')
-            <div class="text-red-500 mt-2 text-small">
-                {{$message}}
-            </div>
-            @enderror
-        </div>
-        <div class="mb-5">
-            <input class="bg-white p-2"
-                   type="text"
-                   name="description"
-                   value="{{old('description')}}"
-                   placeholder="Job Description"
+                Job Info
+            </h2>
+
+            <x-inputs.text id="title"
+                           name="title"
+                           label="Job Title"
+                           placeholder="Software Engineer"
+            />
+
+            <x-inputs.text-area id="description"
+                                name="description"
+                                label="Job Description"
+                                placeholder="We are seeking a skilled and motivated Software Developer to join our growing development team..."
+
+            />
+
+            <x-inputs.text id="salary"
+                           type="number"
+                           name="salary"
+                           label="Salary"
+                           placeholder="90000"
+            />
+
+            <x-inputs.text-area id="requirements"
+                                name="requirements"
+                                label="Requirements"
+                                rows="4"
+                                placeholder="Bachelor's degree in Computer Science"
+            />
+
+            <x-inputs.text-area id="benefits"
+                                name="benefits"
+                                label="Benefits"
+                                rows="4"
+                                placeholder="Competitive salary, flexible working hours, and a great work environment."
+            />
+
+            <x-inputs.text id="tags"
+                           name="tags"
+                           label="Tags (comma-separated)"
+                           placeholder="development, coding, java, python"
+            />
+
+            <x-inputs.select id="job_type"
+                             name="job_type"
+                             label="Job Type"
+                             value="{{old('job_type')}}"
+                             :options="[
+                                        'Full-Time' => 'Full-Time',
+                                        'Part-Time' => 'Part-Time',
+                                        'Contract' => 'Contract',
+                                        'Temporary' => 'Temporary',
+                                        'Internship' => 'Internship',
+                                        'Volunteer' => 'Volunteer',
+                                        'On-Call' => 'On-Call',
+                                       ]"
+            />
+
+            <x-inputs.select id="is_remote"
+                             name="is_remote"
+                             label="Remote"
+                             :options="[
+                                        '0' => 'No',
+                                        '1' => 'Yes',
+                                       ]"
+            />
+
+            <x-inputs.text id="address"
+                           name="address"
+                           label="Address"
+                           placeholder="123 Main St"
+            />
+
+            <x-inputs.text id="city"
+                           name="city"
+                           label="City"
+                           placeholder="Albany"
+            />
+
+            <x-inputs.text id="state"
+                           name="state"
+                           label="State"
+                           placeholder="NY"
+            />
+
+            <x-inputs.text id="zip_code"
+                           name="zip_code"
+                           label="ZIP Code"
+                           placeholder="12201"
+            />
+
+            <h2 class="text-2xl font-bold mb-6 text-center text-gray-500"
             >
-            @error('description')
-            <div class="text-red-500 mt-2 text-small">
-                {{$message}}
-            </div>
-            @enderror
-        </div>
-        <button type="submit">
-            Create
-        </button>
-    </form>
+                Company Info
+            </h2>
+
+            <x-inputs.text id="company_name"
+                           name="company_name"
+                           label="Company Name"
+                           placeholder="Enter Company Name"
+            />
+
+            <x-inputs.text-area id="company_description"
+                                name="company_description"
+                                rows="4"
+                                label="Company Description"
+                                placeholder="Enter Company Description"
+            />
+
+            <x-inputs.text id="company_website"
+                           type="url"
+                           name="company_website"
+                           label="Company Website"
+                           placeholder="Enter Company Website"
+            />
+
+            <x-inputs.text id="contact_phone"
+                           name="contact_phone"
+                           label="Contact Phone"
+                           placeholder="Enter Phone Number"
+            />
+
+            <x-inputs.text id="contact_email"
+                           name="contact_email"
+                           type="email"
+                           label="Contact Email"
+                           placeholder="Email where you want to receive applications"
+            />
+
+            <x-inputs.file id="company_logo"
+                           name="company_logo"
+                           label="Company Logo"
+            />
+
+            <button type="submit"
+                    class="w-full bg-green-500 hover:bg-green-600 text-white px-4 py-2 my-3 rounded focus:outline-none"
+            >
+                Save
+            </button>
+        </form>
+    </div>
 </x-layout>
