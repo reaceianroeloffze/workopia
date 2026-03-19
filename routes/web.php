82891b57e0aa472a3d16766d7acba325eff41974
Reace Ian Roeloffze <reaceianroeloffze@gmail.com>
2026-03-19 14:11:22 +0200

    Add Routes for Login and Registration Pages and Data Submission

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,24 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
 // Route to home page
-Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('homepage');
 
-// Route to jobs
+// Routes to all job-related pages
 Route::resource('jobs', JobController::class);
+
+/*
+    Login and Registration Routes
+*/
+
+// Register page
+Route::get('/register', [RegisterController::class, 'register'])->name('register');
+// Processing registration data
+Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
+// Login page
+Route::get('/login', [LoginController::class, 'login'])->name('login');
+// Authenticate user logins
+Route::post('/login', [LoginController::class, 'authenticate'])->name('login.authenticate');
