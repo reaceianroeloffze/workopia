ae89f2c79dd8b302fb31ed9c6263fd905435ce59
Reace Ian Roeloffze <reaceianroeloffze@gmail.com>
2026-03-20 13:31:44 +0200

    Populate register view with form elements and submit button

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,3 +1,71 @@
 <x-layout>
-    <h1>Register Page</h1>
+    <div class="bg-white
+                rounded-lg
+                shadow-md
+                w-full
+                md:max-w-xl
+                mx-auto
+                mt-12
+                p-8
+                py-12"
+    >
+        <h1 class="text-4xl
+                   text-center
+                   font-bold
+                   mb-10"
+        >
+            Create Account
+        </h1>
+        <form action="{{ route('register.store') }}"
+              method="POST"
+              class="space-y-4"
+        >
+            @csrf
+            <x-inputs.text id="name"
+                           name="name"
+                           placeholder="Full name"
+            />
+            <x-inputs.text id="email"
+                           name="email"
+                           type="email"
+                           placeholder="Email address"
+            />
+            <x-inputs.text id="password"
+                           name="password"
+                           type="password"
+                           placeholder="Password"
+            />
+            <x-inputs.text id="password_confirmation"
+                           name="password_confirmation"
+                           type="password"
+                           placeholder="Confirm password"
+            />
+            <button type="submit"
+                    class="w-full
+                            bg-blue-500
+                            hover:bg-blue-600
+                            text-white
+                            font-bold
+                            py-2
+                            px-4
+                            rounded
+                            focus:outline-none
+                            cursor-pointer"
+            >
+                Create Account
+            </button>
+            <p class="mt-4
+                      text-gray-500
+                      text-center"
+            >
+                Already have an account?
+                <a href="{{route('login')}}"
+                   class="text-blue-900
+                          hover:underline"
+                >
+                    Login here
+                </a>
+            </p>
+        </form>
+    </div>
 </x-layout>
