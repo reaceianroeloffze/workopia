e39039744c554da8c716ac6239b66fb4048f375d
Reace Ian Roeloffze <reaceianroeloffze@gmail.com>
2026-04-13 14:56:49 +0200

    Add conditions to display appropriate nav links when logged in VS when not logged in.

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -20,38 +20,42 @@
             >
                 All jobs
             </x-nav-link>
-            <x-nav-link url="/jobs/saved"
-                        title="Your saved/bookmarked jobs"
-                        :active="request()->is('jobs/saved')"
-            >
-                Saved Jobs
-            </x-nav-link>
-            <x-nav-link url="/login"
-                        title="Login to your Workopia account"
-                        :active="request()->is('login')"
-                        icon="user"
-            >
-                Login
-            </x-nav-link>
-            <x-nav-link url="/register"
-                        title="Create an account on Workopia"
-                        :active="request()->is('register')"
-            >
-                Register
-            </x-nav-link>
-            <x-nav-link url="/dashboard"
-                        title="Go to your Workopia dashboard"
-                        :active="request()->is('/dashboard')"
-                        icon="gauge"
-            >
-                Dashboard
-            </x-nav-link>
-            <x-button-link url="/jobs/create"
-                           title="Create a new job listing"
-                           icon="edit"
-            >
-                Create Job
-            </x-button-link>
+            @auth
+                <x-nav-link url="/jobs/saved"
+                            title="Your saved/bookmarked jobs"
+                            :active="request()->is('jobs/saved')"
+                >
+                    Saved Jobs
+                </x-nav-link>
+                <x-nav-link url="/dashboard"
+                            title="Go to your Workopia dashboard"
+                            :active="request()->is('/dashboard')"
+                            icon="gauge"
+                >
+                    Dashboard
+                </x-nav-link>
+                <x-logout-form/>
+                <x-button-link url="/jobs/create"
+                               title="Create a new job listing"
+                               icon="edit"
+                >
+                    Create Job
+                </x-button-link>
+            @else
+                <x-nav-link url="/login"
+                            title="Login to your Workopia account"
+                            :active="request()->is('login')"
+                            icon="user"
+                >
+                    Login
+                </x-nav-link>
+                <x-nav-link url="/register"
+                            title="Create an account on Workopia"
+                            :active="request()->is('register')"
+                >
+                    Register
+                </x-nav-link>
+            @endauth
         </nav>
         <button id="hamburger"
                 class="text-white
@@ -81,43 +85,48 @@
         >
             All Jobs
         </x-nav-link>
-        <x-nav-link url="/jobs/saved"
-                    :active="request()->is('jobs/saved')"
-                    :mobile="true"
-                    title="Your saved/bookmarked jobs"
-        >
-            Saved Jobs
-        </x-nav-link>
-        <x-nav-link url="/login"
-                    :active="request()->is('login')"
-                    :mobile="true"
-                    icon="user"
-                    title="Login to your Workopia account"
-        >
-            Login
-        </x-nav-link>
-        <x-nav-link url="/register"
-                    :active="request()->is('register')"
-                    :mobile="true"
-                    title="Create an account on Workopia"
-        >
-            Register
-        </x-nav-link>
-        <x-nav-link url="/dashboard"
-                    :active="request()->is('dashboard')"
-                    :mobile="true"
-                    icon="gauge"
-                    title="Go to your Workopia dashboard"
-        >
-            Dashboard
-        </x-nav-link>
-        <x-button-link url="/jobs/create"
-                       title="Create a new job listing"
-                       icon="edit"
-                       :mobile="true"
-                       :block="true"
-        >
-            Create Job
-        </x-button-link>
+        @auth
+            <x-nav-link url="/jobs/saved"
+                        :active="request()->is('jobs/saved')"
+                        :mobile="true"
+                        title="Your saved/bookmarked jobs"
+            >
+                Saved Jobs
+            </x-nav-link>
+            <x-nav-link url="/dashboard"
+                        :active="request()->is('dashboard')"
+                        :mobile="true"
+                        icon="gauge"
+                        title="Go to your Workopia dashboard"
+            >
+                Dashboard
+            </x-nav-link>
+            <x-logout-form/>
+            <div class="pt-2"></div>
+            <x-button-link url="/jobs/create"
+                           title="Create a new job listing"
+                           icon="edit"
+                           :mobile="true"
+                           :block="true"
+            >
+                Create Job
+            </x-button-link>
+        @else
+            <x-nav-link url="/login"
+                        :active="request()->is('login')"
+                        :mobile="true"
+                        icon="user"
+                        title="Login to your Workopia account"
+            >
+                Login
+            </x-nav-link>
+            <x-nav-link url="/register"
+                        :active="request()->is('register')"
+                        :mobile="true"
+                        title="Create an account on Workopia"
+            >
+                Register
+            </x-nav-link>
+        @endauth
     </nav>
 </header>
