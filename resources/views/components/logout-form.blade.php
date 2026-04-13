0515f670c2f62157cb4ae35a19613f10a87fb3d2
Reace Ian Roeloffze <reaceianroeloffze@gmail.com>
2026-04-13 15:02:31 +0200

    Create Logout form component

---
+++ resources/views/components/logout-form.blade.php
@@ -0,0 +1,9 @@
+<form action="{{route('logout')}}" method="post">
+    @csrf
+    <button type="submit"
+            class="text-white
+            cursor-pointer"
+    >
+        <i class="fa fa-sign-out"></i> Logout
+    </button>
+</form>
