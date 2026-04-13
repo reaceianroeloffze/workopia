bba69064c613fed6fdfff85dd1f550390603a7da
Reace Ian Roeloffze <reaceianroeloffze@gmail.com>
2026-04-13 15:04:07 +0200

    Remove unnecessary use statement

--- app/Http/Controllers/LogoutController.php
+++ app/Http/Controllers/LogoutController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Auth;
 
 class LogoutController extends Controller
