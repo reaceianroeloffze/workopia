c8ec16951de02ca3debba2606e863a6e1e67eb3f
Reace Ian Roeloffze <reaceianroeloffze@gmail.com>
2026-04-14 11:31:28 +0200

    Display Edit and Delete buttons only for jobs users upload/own

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -21,40 +21,42 @@
                         <i class="fa fa-arrow-alt-circle-left"></i>
                         Back To Listings
                     </a>
-                    <div class="flex
+                    @can('update', $job)
+                        <div class="flex
                                 space-x-3
                                 ml-4"
-                    >
-                        <a href="{{route('jobs.edit', $job->id)}}"
-                           class="px-4
+                        >
+                            <a href="{{route('jobs.edit', $job->id)}}"
+                               class="px-4
                                   py-2
                                   bg-blue-500
                                   hover:bg-blue-600
                                   text-white
                                   rounded"
-                        >
-                            Edit
-                        </a>
-                        <!-- Delete Form -->
-                        <form method="POST"
-                              action="{{route('jobs.destroy', $job->id)}}"
-                              onsubmit="return confirm('Are you sure you want to delete job listing {{$job->title}}?');"
-                        >
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit"
-                                    class="px-4
+                            >
+                                Edit
+                            </a>
+                            <!-- Delete Form -->
+                            <form method="POST"
+                                  action="{{route('jobs.destroy', $job->id)}}"
+                                  onsubmit="return confirm('Are you sure you want to delete job listing {{$job->title}}?');"
+                            >
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit"
+                                        class="px-4
                                            py-2
                                            bg-red-500
                                            hover:bg-red-600
                                            text-white
                                            rounded"
-                            >
-                                Delete
-                            </button>
-                        </form>
-                        <!-- End Delete Form -->
-                    </div>
+                                >
+                                    Delete
+                                </button>
+                            </form>
+                            <!-- End Delete Form -->
+                        </div>
+                    @endcan
                 </div>
                 <div class="p-4">
                     <h2 class="text-xl
