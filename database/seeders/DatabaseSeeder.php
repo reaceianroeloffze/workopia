77907e64bcf44166789beba379893ed772e26643
Reace Ian Roeloffze <reaceianroeloffze@gmail.com>
2026-04-14 11:40:07 +0200

    Create test user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,7 @@ class DatabaseSeeder extends Seeder
         DB::table('users')->truncate();
         DB::table('job_listings')->truncate();
 
+        $this->call(TestUserSeeder::class);
         $this->call(RandomUserSeeder::class);
         $this->call(JobSeeder::class);
     }
