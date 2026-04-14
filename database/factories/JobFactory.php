cb98d0de4041095f78e23640c52b892b2edd2e96
Reace Ian Roeloffze <reaceianroeloffze@gmail.com>
2026-04-14 11:45:36 +0200

    Organise use statements

--- database/factories/JobFactory.php
+++ database/factories/JobFactory.php
@@ -3,8 +3,8 @@
 namespace Database\Factories;
 
 use App\Models\Job;
-use Illuminate\Database\Eloquent\Factories\Factory;
 use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
  * @extends Factory<Job>
