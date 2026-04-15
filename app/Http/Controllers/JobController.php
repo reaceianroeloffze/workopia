416cdbeb967d51bf976ac367758398fa82a1b6f3
Reace Ian Roeloffze <reaceianroeloffze@gmail.com>
2026-04-15 13:49:53 +0200

    Enable pagination on jobs/index, include various pagination style files

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -24,7 +24,7 @@ class JobController extends Controller
      * */
     public function index(): View
     {
-        $jobs = Job::all();
+        $jobs = Job::paginate(9);
 
         return view(
             'jobs.index'
--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -1,10 +1,14 @@
 <x-layout>
     <x-slot name="title">Jobs</x-slot>
-    <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
+    {{-- Pagination Links --}}
+    {{$jobs->links()}}
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mb-6">
         @forelse ($jobs as $job)
             <x-job-card :job="$job"/>
         @empty
             <p>No Available Jobs</p>
         @endforelse
     </div>
+    {{-- Pagination Links --}}
+    {{$jobs->links()}}
 </x-layout>
