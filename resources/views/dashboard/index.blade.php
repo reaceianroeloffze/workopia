da993c28ba6ba066cce2b3ae1aa531bef93020eb
Reace Ian Roeloffze <reaceianroeloffze@gmail.com>
2026-04-15 13:41:58 +0200

    Display user avatar on the dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -14,32 +14,82 @@
             >
                 Profile Info
             </h2>
+
+            @if($user->avatar)
+                <div class="mt-2 flex justify-center">
+                    <img src="{{asset('storage/' . $user->avatar)}}"
+                         alt="{{$user->name}}"
+                         class="w-32
+                                h-32
+                                object-cover
+                                rounded-full">
+                </div>
+            @endif
+
+            <form action="{{route('profile.update')}}"
+                  method="POST"
+                  enctype="multipart/form-data"
+            >
+                @csrf
+                @method('PUT')
+
+                <x-inputs.text id="name"
+                               label="Name"
+                               name="name"
+                               :value="$user->name"
+                />
+                <x-inputs.text id="email"
+                               label="Email Address"
+                               type="email"
+                               name="email"
+                               :value="$user->email"
+                />
+                <x-inputs.file id="avatar"
+                               label="Upload Avatar"
+                               name="avatar"
+                />
+                <button type="submit"
+                        class="w-full
+                               bg-green-500
+                               hover:bg-green-600
+                               text-white
+                               px-4
+                               py-2
+                               border
+                               rounded
+                               cursor-pointer
+                               focus:outline-none"
+                >
+                    Save
+                </button>
+            </form>
         </section>
 
         {{-- Job Listings --}}
         <section class="bg-white
-                    p-8
-                    rounded-lg
-                    shadow-md
-                    w-full"
+                        p-8
+                        rounded-lg
+                        shadow-md
+                        w-full"
         >
             <h2 class="text-3xl
-                   text-center
-                   font-bold
-                   mb-4"
+                       text-center
+                       font-bold
+                       mb-4"
             >
                 My Job Listings
             </h2>
             @forelse($jobs AS $job)
                 <div class="flex
                             flex-col
-                            items-center
+                            md:items-center
                             md:flex-row
                             md:justify-between
                             border-b-2
                             border-gray-200
                             py-2"
                 >
+                    {{-- Job Listing --}}
                     <section>
                         <h3 class="text-xl
                                    font-semibold"
@@ -47,25 +97,33 @@
                             {{$job->title}}
                         </h3>
                         <p class="text-gray-700
-                                 mb-4
-                                 md:mb-0">
+                                     mb-4
+                                     md:mb-0">
                             {{$job->job_type}}
                         </p>
                     </section>
+                    {{-- Actions --}}
                     <div class="flex space-x-3">
+                        {{-- Edit Button --}}
                         <a href="{{route('jobs.edit', $job->id)}}"
                            class="bg-blue-500
-                              text-white
-                              px-4
-                              py-2
-                              rounded
-                              text-sm"
+                                  text-white
+                                  text-center
+                                  px-4
+                                  py-2
+                                  rounded
+                                  text-sm
+                                  w-1/2
+                                  md:w-auto"
                         >
                             Edit
                         </a>
+                        {{-- Delete Form --}}
                         <form method="POST"
                               action="{{route('jobs.destroy', $job->id)}}?from=dashboard"
                               onsubmit="return confirm('Are you sure you want to delete job listing {{$job->title}}?');"
+                              class="w-1/2
+                                     md:w-auto"
                         >
                             @csrf
                             @method('DELETE')
@@ -77,7 +135,9 @@
                                            text-white
                                            rounded
                                            text-sm
-                                           cursor-pointer"
+                                           cursor-pointer
+                                           w-full
+                                           md:w-auto"
                             >
                                 Delete
                             </button>
