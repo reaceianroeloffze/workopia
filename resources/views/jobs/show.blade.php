f78a9b14f7de97f8e24dfa8822c51a8d9c426b28
Reace Ian Roeloffze <reaceianroeloffze@gmail.com>
2026-04-16 09:29:03 +0200

    Add logic for displaying bookmark button based on user authentication

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -210,26 +210,45 @@
                     Visit Website
                 </a>
             @endif
-            <a href=""
-               class="mt-10
-                      bg-blue-500
-                      hover:bg-blue-600
-                      text-white
-                      font-bold
-                      w-full
-                      py-2
-                      px-4
-                      rounded-full
-                      flex
-                      items-center
-                      justify-center"
-            >
-                <i class="fa
-                          fa-bookmark
-                          mr-3"
-                ></i> Bookmark
-                Listing
-            </a>
+            {{-- Bookmark Button --}}
+
+            @guest
+                <p class="mt-10
+                          bg-gray-200
+                          text-gray-700
+                          font-bold
+                          w-full
+                          py-2
+                          px-4
+                          rounded-full
+                          text-center"
+                >
+                    <i class="fa fa-info-circle mr-3"></i> You must be logged in to bookmark a job.
+                </p>
+            @else
+                <form action="{{route('bookmarks.store', $job->id)}}"
+                      method="POST"
+                      class="mt-10"
+                >
+                    @csrf
+                    <button class="bg-blue-500
+                                   hover:bg-blue-700
+                                   text-white
+                                   font-bold
+                                   w-full
+                                   py-2
+                                   px-4
+                                   rounded-full
+                                   flex
+                                   items-center
+                                   justify-center
+                                   ease-in-out
+                                   duration-200"
+                    >
+                        <i class="fa fa-bookmark mr-3"></i> Bookmark This Listing
+                    </button>
+                </form>
+            @endguest
         </aside>
     </div>
 </x-layout>
