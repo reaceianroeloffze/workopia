673c8b453ce8c84a0695e1ced183915d95c767c8
Reace Ian Roeloffze <reaceianroeloffze@gmail.com>
2026-04-16 20:13:58 +0200

    Truncate applicants table after seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,7 @@ class DatabaseSeeder extends Seeder
         DB::table('users')->truncate();
         DB::table('job_listings')->truncate();
         DB::table('job_user_bookmarks')->truncate();
+        DB::table('applicants')->truncate();
 
         $this->call(TestUserSeeder::class);
         $this->call(RandomUserSeeder::class);
