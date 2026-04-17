be84e61ee737a4b577415fdb2a01d4667d3cfbf6
Reace Ian Roeloffze <reaceianroeloffze@gmail.com>
2026-04-17 22:36:54 +0200

    Format code

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -53,15 +53,15 @@
                             <img src="{{asset('storage/' . Auth::user()->avatar)}}"
                                  alt="{{Auth::user()->name}}"
                                  class="w-10
-                                    h-10
-                                    rounded-full"
+                                        h-10
+                                        rounded-full"
                             >
                         @else
                             <img src="{{asset('storage/avatars/default-avatar.png')}}"
                                  alt="{{Auth::user()->name}}"
                                  class="w-10
-                                    h-10
-                                    rounded-full"
+                                        h-10
+                                        rounded-full"
                             >
                         @endif
                     </a>
